0980460f6474554414d93bfe7eaa056bab9ad5ec
Arun Dube <arund@chetu.com>
2020-11-04 19:00:23 +0530

    Complete Code

--- src/LeftBar.js
+++ src/LeftBar.js
@@ -1,25 +1,83 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import RightBar from './RightBar';
 
 function LeftBar() {
+  const [color, setColor] = useState({
 
+    data: [{
+    
+    id: 'PRODUCT_ID_1', name: 'Product1', brand: 'Brand1',color: "red", Variant: [{
+    type: 'default',
+    
+    variationId: 'PRODUCT_ID_1_VAR_ID_0',    
+    cost: 123, color: 'red',
+    months: ['November', 'December', 'January']
+    
+    }, {
+    
+    type: 'variation',
+    
+    variationId: 'PRODUCT_ID_1_VAR_ID_1', cost: 106,
+    color: 'orange',
+    
+    months: ['November', 'December', 'January']
+    
+    }]
+    
+    }, {
+    
+    id: 'PRODUCT_ID_2',
+    
+    name: 'Product2', 
+    color: "green",
+    
+    brand: 'Brand2', Variant: [{
+    type: 'default',
+    
+    variationId: 'PRODUCT_ID_2_VAR_ID_0',
+    
+    cost: 225, color: 'green',
+    months: ['January', 'Feburary']
+    
+    }, {
+    
+    type: 'variation',
+    
+    variationId: 'PRODUCT_ID_2_VAR_ID_1', cost: 350,
+    color: 'purple',
+    
+    months: [ 'January', 'February', 'March']    
+    }]    
+    }]    
+    });
+    const [activeColor, setActiveColor] = useState();
+    const updateColor = (data) => {
+      setActiveColor([data]);
+    }
+    
   return (
     <div>
-        <div style={{width: '50%', float: 'left'}}>
-            <h3>Portugal</h3>
-            <input type="checkbox" label="Hello" /> 
-            <label>Hello</label>
-            <h3>Nicaragua</h3>
-            <input type="checkbox" label="Hello" /> 
-            <label>Hello</label>
-            <h3>Marshel Island</h3>
-            <input type="checkbox" label="Hello" /> 
-            <label>Hello 3</label>
+      <div style={{ width: '30%', padding: '5px', float: 'left', border: '1px solid #000' }}>
+        <h3>Filters</h3>
+
+        <div style={{ padding: '10px', border: '1px solid #000' }}>
+          <h3>Color</h3>
+          <div>
+          <div style={{ padding: '10px', border: '1px solid #000' }}>
+            {color && color.data && color.data.length>0 ? color.data.map(data=>{
+              return <div onClick={()=>updateColor(data)} style={{background:`${data.color}`, borderRadius: "20px", width: "20px", margin:"10px", height: "20px"}}>
+              <span style={{marginLeft:'25px'}} >{data.color}</span>
+            </div>;
+            }):''}
+          
+          </div>
         </div>
-        <div style={{width: '50%', float: 'right'}}>
-            <RightBar />
-       </div>
-      
+        </div>
+      </div>
+      <div style={{ width: '69%', float: 'right' }}>
+        <RightBar activeColor={activeColor} color={color}/>
+      </div>
+
     </div>
   );
 }
--- src/RightBar.js
+++ src/RightBar.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function RightBar() {
+function RightBar(props) {
+  const [Variant, setVariant] = useState();
+  const { activeColor } = props;
+ 
+
+  const renderColor = (data) => {
+    return <div style={{ padding: '10px', border: '1px solid #000', marginTop: '10px', height: '185px' }}>
+      <div style={{ background: `${(Variant && Variant.color) || data.color}`, width: "150px", height: "150px", float: "left" }}>
+
+      </div>
+      <span style={{ marginLeft: '25px', float: "left" }}>
+        <p>Product Name: {data.name}</p>
+        <p>By: {data.name}</p>
+        <p>Price: {(Variant && Variant.cost) || 'Cost'}</p>
+        {data.Variant && data.Variant.length ? data.Variant.map(value => {
+          return <div onClick={() => updateVariant(value)} style={{ background: `${value.color}`, borderRadius: "20px", width: "20px", margin: "10px", height: "20px" }}>
+            <span style={{ marginLeft: '25px' }} >{value.color}</span>
+          </div>;
+        }) : ""}
+
+      </span>
+    </div>;
+  }
+
+  const updateVariant = (data) => {
+    setVariant(data);
+  }
+
   return (
     <div>
-      <h3>Heading</h3>
-      <input type="checkbox" label="Hello" /> 
-      <label>Hello</label>
-      <h3>Heading 1</h3>
-      <input type="checkbox" label="Hello" /> 
-      <label>Hello</label>
-      <h3>Heading 2</h3>
-      <input type="checkbox" label="Hello" /> 
-      <label>Hello 3</label>
+      <div style={{ padding: '10px', border: '1px solid #000' }}>
+        {activeColor && activeColor.length > 0 ? activeColor.map(data => renderColor(data)) : props.color.data.map(data => renderColor(data))}
+      </div>
     </div>
   );
 }
